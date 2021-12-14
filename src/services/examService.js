b0500ca7bb54b175a1ba223872421aa4e31e6ebc
Viet Dinh <viet.dinh@sioux.asia>
2021-12-14 10:16:37 +0700

    add analyze result

--- src/services/examService.js
+++ src/services/examService.js
@@ -52,6 +52,10 @@ const examService = {
     let totalMark = 0;
     let message;
     let courses = [];
+    let nonDocQuestion = 0;
+    let nAnswerNonDocQuestion = 0;
+    let docQuestion = 0;
+    let nAnswerDocQuestion = 0;
 
     const exam = await examRepository.getExamById(id);
 
@@ -60,9 +64,13 @@ const examService = {
         const question = await questionRepository.getQuestionById(questionId._id);
 
         if (question) {
+          if (question.questionDocumentId) docQuestion++;
+          else nonDocQuestion++;
           totalMark = totalMark + question.mark;
           data.forEach(value => {
             if (value._id === question._id.toString() && value.answer === question.answerRight) {
+              if (question.questionDocumentId) nAnswerDocQuestion++;
+              else nAnswerNonDocQuestion++;
               mark = mark + question.mark;
             }
           })
@@ -73,6 +81,8 @@ const examService = {
     const resultRatio = mark * 1000 / totalMark;
 
     const realMark = getDozen(resultRatio);
+
+    const analyzeMessage = analyze(nAnswerNonDocQuestion, nonDocQuestion, nAnswerDocQuestion, docQuestion, realMark);
 
     const courseLevel = await levelRepository.getLevelByMark(realMark + 50);
 
@@ -85,6 +95,7 @@ const examService = {
       status: Status.Success,
       result: {
         realMark,
+        analyzeMessage,
         courses,
         message,
       },
@@ -189,4 +200,36 @@ const getDozen = (n) => {
   var r = n%10;
   if(r>4) return Math.ceil(n/10) *10;
   else return Math.floor(n/10) *10;
+}
+
+const analyze = (choices, nChoices, doc, nDoc, mark) => {
+  let choiceAnalyze = ""; 
+  let docAnalyze = "";
+  let markAnalyze = "";
+  if (choices === nChoices / 2)
+    choiceAnalyze = `khả năng làm trắc nghiệm của bạn ở mức trung bình với ${choices} câu dúng trên tổng thể ${nChoices} câu hỏi`
+  else if (choices === nChoices)
+    choiceAnalyze = `Chúc mừng bạn làm đúng hết tất cả các câu trắc nghiệm ^_^`
+  else if (choices > nChoices/2 && choices < nChoices)
+    choiceAnalyze = `Bạn làm đúng được hơn nữa số câu trắc nghiệm với ${choices} câu đúng trên ${nChoices} câu`
+  else if (choices < nChoices/2)
+    choiceAnalyze = `Bạn cần cải thiện thêm vì chỉ đúng chưa tới một nữa số câu trắc nghiệm với ${choices} câu đúng trên ${nChoices} câu`
+
+  if (doc === nDoc / 2)
+    docAnalyze = `khả năng đọc và nghe của bạn ở mức trung bình với ${doc} câu đúng trên tổng thể ${nDoc} câu hỏi`
+  else if (doc === nDoc)
+    docAnalyze = `Chúc mừng bạn làm đúng hết tất cả các câu đọc và nghe ^_^`
+  else if (doc > nDoc/2 && doc < nDoc)
+    docAnalyze = `Bạn làm đúng được hơn 1/2 số câu đọc và nghe với ${doc} câu đúng trên ${nDoc} câu`
+  else if (doc < nDoc/2)
+    docAnalyze = `Bạn cần cải thiện thêm vì chỉ đúng chưa tới một nữa số câu của đọc và nghe với ${doc} câu đúng trên ${nDoc} câu`
+  
+  if (mark === 1000) 
+    markAnalyze = `Bạn thật giỏi, làm đúng hết đạt 1000/1000`
+  else if (mark > 500 && mark < 1000)
+    markAnalyze = `Khả năng tiếng anh của bạn rất khá có thể cải thiện thêm với các khóa học thuộc mức độ cao cấp và siêu cấp`
+  else if (mark < 500)
+    markAnalyze = 'Tiếng Anh của bạn chỉ đạt mức sơ cấp hãy xem thêm các khóa học phía dưới để cải thiện thêm'
+
+  return `${choiceAnalyze}-${docAnalyze}-${markAnalyze}`
 }
